7f46605b6e0b3409aa6203fff64dce27f481ec37
Apitpr0 <apitpro123@gmail.com>
2023-03-06 11:29:41 +0800

    fix avatar not showing

--- src/app/(user)/(pages)/globalComponents/navbar/navbar.tsx
+++ src/app/(user)/(pages)/globalComponents/navbar/navbar.tsx
@@ -9,7 +9,7 @@ function navbar() {
   <div className="container flex flex-wrap items-center justify-between mx-auto">
     <a href="https://flowbite.com/" className="flex items-center">
         <Image src={indexAssets.schoolLogo  } className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">SKLY Content Management System</span>
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">SKLY Counselling Management System</span>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
@@ -32,9 +32,9 @@ function navbar() {
         <li>
           <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Hubungi Kami</a>
         </li>
-        <li>
-        <Avatar alt="BobRamen" src={indexAssets.profilepic } />
-        </li>
+        <Avatar>
+        <Image alt="BobRamen" src={indexAssets.profilepic } alt={""}/>
+        </Avatar>
       </ul>
     </div>
   </div>
