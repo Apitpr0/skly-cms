8dfd9f498609c9737c84909470f06a0b880b3aa8
Apitpr0 <apitpro123@gmail.com>
2023-03-08 11:22:09 +0800

    add basic register/login php

--- php_code/login.php
+++ php_code/login.php
@@ -20,13 +20,46 @@
             //verify password here with hash
             if(password_verify($password,$passDB))
             {
-                $msg="Log In !";
+                $msg="Berjaya log masuk, Selamat datang";
             }
         }
         else
         {
-            $msg="Check Your Input";
+            $msg="Salah, periksa kembali";
         }
     } 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Daftar Masuk</title>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+ <div class="container" style="margin-top: 100px">
+  <div class="row justify-content-center">
+   <div class="col-md-6 col-md-offset-3" align="center">
+    
+    <img src=""><br><br>
 
+    <h4>Selamat Kembali, sila daftar masuk</h4><br>
+
+    <?php 
+     if($msg != "")
+      echo $msg; 
+
+    ?> 
+
+    <form method="post" action="login.php">
+     <input class="form-control" type="" name="ic" placeholder="IC"><br>
+     <input class="form-control" type="password" name="password" placeholder="Password"><br>
+     <input class="btn btn-primary" type="submit" name="submit" placeholder="Login"><br>
+    </form>
+
+   </div>
+  </div>
+ </div>
+
+</body>
+</html>
--- php_code/register.php
+++ php_code/register.php
@@ -3,8 +3,6 @@ require('db_connection.php'); //pointing to our db connection
 $msg="";
 if(isset($_POST['submit'])) //if someone press submit
 {
-
-    $id=$_POST['id'];
     $ic=$_POST['ic'];
     $password=$_POST['password'];
     $cPass=$_POST['cpass'];
@@ -17,10 +15,44 @@ else
 {
     //hash password
     $passwordHash=password_hash($password,PASSWORD_DEFAULT);
-    $query="INSERT INTO USERS (name,email,password) VALUES ('$id','$ic','$passwordHash')";
+    $query="INSERT INTO USERS (ic,password) VALUES ($ic','$password')";
     mysqli_query($connection,$query) or die(mysqli_error($connection));
     $msg="Register Successful";
 }
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Pendaftaran</title>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+ <div class="container" style="margin-top: 100px">
+  <div class="row justify-content-center">
+   <div class="col-md-8 col-md-offset-3" align="center">
+    
+
+    <h4>Register</h4><br>
+
+    <?php 
+     if($msg != "")
+      echo $msg; 
+
+    ?>
+
+    <form method="post" action="register.php">
+     <input class="form-control" type="textbox" name="ic" placeholder="IC" required="true"><br>
+     <input class="form-control" type="password" name="password" placeholder="Password" required="true"><br>
+     <input class="form-control" type="password" name="cPass" placeholder="Confirm Password" required="true"><br>
+     <input class="btn btn-primary" type="submit" name="submit" placeholder="Submit"><br>
+    </form>
+
+   </div>
+  </div>
+ </div>
+
+</body>
+</html>
 
