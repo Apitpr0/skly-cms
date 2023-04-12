df64219b1a30edf33e2c3e79249b315b41caa606
Apitpr0 <apitpro123@gmail.com>
2023-04-12 23:45:38 +0800

    change value passed to dashboard and full update done

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -84,7 +84,7 @@ $ic = $_SESSION['ic'];
               <td class="text-base text-white font-medium px-6 py-4 whitespace-nowrap">
 <div class="flex space-x-2 justify-center">
   <div>
-    <button type="button" class="inline-block px-6 py-2.5 bg-blue-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"><a href="update.php?update_id=<?php echo $info[
+    <button type="button" class="inline-block px-6 py-2.5 bg-blue-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"><a href="update.php?appt_id=<?php echo $info[
         "appt_id"
     ]; ?>">Kemaskini</a></button>
     <button type="button" data-modal-toggle="staticModal" class="inline-block px-6 py-2.5 bg-red-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">
--- update.php
+++ update.php
@@ -1,66 +1,64 @@
 <?php
-include('Components/db/db_connection.php');
-include('Components/header.php');
-include('Components/navbar.php');
+include 'components/db/db_connection.php';
+if(isset($_POST['submit'])) {
+    $appt_id = $_POST['appt_id'];
+    $name = $_POST['name'];
+    $appt_date = $_POST['appt_date'];
 
-if(isset($_POST["update"])) {
-    $id = $_POST["appt_id"];
-    $date_t = $_POST["date_time"];
-    $counsellor = $_POST["name"];
-    
-    $result = mysqli_query(
-        $connection,
-        "UPDATE appointment SET appointment_date='$date_t', name='$counsellor' WHERE appt_id='$id'"
-    );
-    
-    if($result) { 
-        ?>
-        <div class="bg-green-200 text-green-800 rounded-md p-4 mb-4">
-            Appointment updated successfully!
-        </div>
-        <?php 
-    } else { 
-        ?>
-        <div class="bg-red-200 text-red-800 rounded-md p-4 mb-4">
-            Failed to update appointment!
-        </div>
-        <?php 
+    // update the appointment record
+    $sql = "UPDATE appointment SET name='$name', appointment_date='$appt_date' WHERE appt_id='$appt_id'";
+    $result = mysqli_query($connection, $sql);
+
+    if($result) {
+        echo "Appointment record updated successfully!";
+    } else {
+        echo "Error updating appointment record: " . mysqli_error($connection);
     }
 }
-
-if (isset($_GET['appt_id'])) {
-  $appt_id = $_GET['appt_id'];
-  $result = mysqli_query($connection, "SELECT * FROM appointment WHERE appt_id='$appt_id'");
-  $res = mysqli_fetch_array($result);
-  $date = $res["appointment_date"];
-  $counsellor = $res["name"];
-} 
-
 ?>
 
-<div class="p-8 m-8 bg-white rounded-lg">
-    <button type="button" class="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">
-        <a href="dashboard_admin.php">Kembali ke admin</a>
-    </button>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kemaskini Temujanji</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/tailwind.min.css">
+</head>
+<body>
+    <div class="max-w-md mx-auto py-4">
+        <h1 class="text-2xl font-bold mb-4">Kemaskini Temujanji</h1>
+        <?php
+        if(isset($_GET['appt_id'])) {
+            $appt_id = $_GET['appt_id'];
+            $sql = "SELECT * FROM appointment WHERE appt_id='$appt_id'";
+            $result = mysqli_query($connection, $sql);
 
-<div class="flex items-center justify-center">
-    <div class="px-8 py-6 mt-20 text-left bg-white shadow-lg rounded-lg">
-        <h3 class="text-2xl font-bold">Kemaskini Temujanji</h3>
-        <form method="post">
-            <input type="hidden" name="id" value="<?php echo $appt_id; ?>">
-            <div class="mt-4">
-                <label class="block">Tarikh dan Masa</label>
-                <input name="date_time" type="datetime-local" required class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" value="<?php echo date('Y-m-d\TH:i', strtotime($date . ' ' . $time)); ?>">
-            </div>
-            <div class="mt-4">
-                <label class="block">Kaunselor</label>
-                <input name="counsellor" type="text" required class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" value="<?php echo $counsellor; ?>">
-
-            </div>
-            <div class="mt-8">
-                <button type="submit" name="update" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Kemaskini</button>
-            </div>
-        </form>
+            if(mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+                ?>
+                <form method="post" action="">
+                    <input type="hidden" name="appt_id" value="<?php echo $row['appt_id']; ?>">
+                    <div class="mb-4">
+                        <label class="block text-gray-700 font-bold mb-2" for="name">Nama Kaunselor:</label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" id="name" value="<?php echo $row['name']; ?>">
+                    </div>
+                    <div class="mb-4">
+                        <label class="block text-gray-700 font-bold mb-2" for="appt_date">Tarikh Temujanji:</label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="datetime-local" name="appt_date" id="appt_date" value="<?php echo date('Y-m-d\TH:i', strtotime($row['appointment_date'])); ?>">
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" name="submit" value="Kemaskini">
+                        <a href="dashboard_admin.php" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Kembali ke paparan admin</a>
+                    </div>
+                </form>
+                <?php
+            } else {
+                echo "Tiada rekod temujanji dijumpai.";
+            }
+        }
+        else {
+            echo "Invalid request.";
+        }
+        ?>
     </div>
-</div>
+</body>
+</html>
