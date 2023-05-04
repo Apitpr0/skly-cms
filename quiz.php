22e8c6e53382f272f2c4a331e310e23de6467615
Apitpr0 <apitpro123@gmail.com>
2023-05-04 22:39:39 +0800

    modify quiz to use scale and add some advice

--- quiz.php
+++ quiz.php
@@ -11,58 +11,71 @@ include('components/navbar.php');
             $quiz_questions = array(
                 array(
                     "question" => "Bagaimana anda biasanya mengatasi situasi yang menimbulkan stres atau kecemasan di sekolah?",
-                    "options" => array("Saya mencuba untuk menghindari situasi tersebut", "Saya bercakap dengan kawan-kawan saya tentang situasi tersebut", "Saya mencari sokongan dari keluarga saya", "Saya cuba untuk bertenang dan bernafas dalam-dalam, dan saya bercakap dengan guru atau kaunselor sekolah saya jika saya memerlukan bantuan"),
-                    'answer_key' => '1',
-                    'score' => 1
+                    "score" => 1
                 ),
                 array(
                     "question" => "Seberapa sering anda bercakap dengan kawan atau keluarga anda tentang masalah-masalah pribadi anda?",
-                    "options" => array("Jarang Sekali", "Kadang-Kadang", "Sering", "Selalu"),
-                    'answer_key' => '2',
-                    'score' => 1
+                    "score" => 1
                 ),
                 array(
                     "question" => "Apa yang anda lakukan jika anda merasa tidak nyaman atau tidak aman di sekolah?",
-                    "options" => array("Saya mencuba untuk menghindari situasi tersebut", "Saya bercakap dengan rakan-rakan saya tentang keadaan itu.", "Saya mencari sokongan dari keluarga saya", "Saya bercakap dengan guru atau orang dewasa yang dipercayai di sekolah untuk mendapatkan bantuan"),
-                    'answer_key' => '3',
-                    'score' => 1
+                    "score" => 1
                 ),
                 array(
                     "question" => "Bagaimana anda rancang dan mengatur waktu anda untuk menyelesaikan tugas-tugas sekolah?",
-                    "options" => array("Saya menyelesaikan tugas-tugas saya secepat mungkin, tanpa perlu rancang atau mengatur waktu", "Saya membuat jadual untuk menyelesaikan tugas-tugas saya", "Saya menyelesaikan tugas-tugas saya pada minit terakhir", "Saya meminta bantuan orang lain untuk menyelesaikan tugas-tugas saya"),
-                    'answer_key' => '4',
-                    'score' => 1
+                    "score" => 1
                 ),
                 array(
                     "question" => "Apakah anda biasanya mengambil tanggung jawab untuk tugas-tugas sekolah atau lebih suka bergantung dengan orang lain?",
-                    "options" => array("Saya biasanya mengambil tanggung jawab untuk tugas-tugas sekolah", "Saya lebih suka bergantung kepada orang lain untuk menyelesaikan tugas-tugas saya", "Saya mengambil tanggung jawab untuk beberapa tugas, tetapi bergantung kepada orang lain untuk yang lainnya", "Saya tidak suka menyelesaikan tugas-tugas sekolah"),
-                    'answer_key' => '5',
-                    'score' => 1
+                    "score" => 1
                 )
             );
+
             foreach ($quiz_questions as $key => $question) {
                 echo '<div class="my-4">';
                 echo '<p class="font-bold">' . $question['question'] . '</p>';
-                foreach ($question['options'] as $option_key => $option) {
-                    echo '<label class="inline-flex items-center mt-2 ml-4">';
-                    echo '<input type="radio" class="form-radio text-indigo-600" name="answer[' . $key . ']" value="' . $option_key . '">';
-                    echo '<span class="ml-2">' . $option . '</span>';
-                    echo '</label>';
-                }
+                echo '<label class="inline-flex items-center mt-2 ml-4">';
+                echo '<input type="radio" class="form-radio text-indigo-600" name="answer[' . $key . ']" value="1">';
+                echo '<span class="ml-2">1</span>';
+                echo '</label>';
+                echo '<label class="inline-flex items-center mt-2 ml-4">';
+                echo '<input type="radio" class="form-radio text-indigo-600" name="answer[' . $key . ']" value="2">';
+                echo '<span class="ml-2">2</span>';
+                echo '</label>';
+                echo '<label class="inline-flex items-center mt-2 ml-4">';
+                echo '<input type="radio" class="form-radio text-indigo-600" name="answer[' . $key . ']" value="3">';
+                echo '<span class="ml-2">3</span>';
+                echo '</label>';
+                echo '<label class="inline-flex items-center mt-2 ml-4">';
+                echo '<input type="radio" class="form-radio text-indigo-600" name="answer[' . $key . ']" value="4">';
+                echo '<span class="ml-2">4</span>';
+                echo '</label>';
+                echo '<label class="inline-flex items-center mt-2 ml-4">';
+                echo '<input type="radio" class="form-radio text-indigo-600" name="answer[' . $key . ']" value="5">';
+                echo '<span class="ml-2">5</span>';
+                echo '</label>';
                 echo '</div>';
             }
+
             ?>
-            <button class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded" type="submit" name="submit">Submit Quiz</button>
+            <button class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded" type="submit" name="submit">Hantar</button>
         </form>
+
         <?php
         if (isset($_POST['submit'])) {
             $user_answers = $_POST['answer'];
             $score = 0;
             foreach ($user_answers as $key => $answer) {
-                if (isset($quiz_questions[$key]) && $answer == $quiz_questions[$key]['answer_key']) {
-                    $score += $quiz_questions[$key]['score'];
+                if (isset($quiz_questions[$key])) {
+                    $score += $answer;
                 }
             }
+            if ($score < 10) {
+                echo "<p class='mt-4 font-bold'>Anda mungkin perlu mencari bantuan dari teman atau orang dewasa untuk mengatasi stres anda.</p>";
+            } else if ($score < 15) {
+                echo "<p class='mt-4 font-bold'>Anda memiliki beberapa strategi yang baik untuk mengatasi stres dan kecemasan, tetapi mungkin masih perlu beberapa bantuan dari waktu ke waktu.</p>";
+            } else {
+                echo "<p class='mt-4 font-bold'>Anda memiliki strategi yang baik untuk mengatasi stres dan kecemasan, dan mampu mengatasi situasi yang menantang dengan baik.</p>";
+            }
         }
-
         ?>
