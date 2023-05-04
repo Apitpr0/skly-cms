7e2835eae92d376bc5569e63c2e2a0f4eb66732c
THEALIHAKER1 <alifhaker1@gmail.com>
2023-05-05 00:14:13 +0800

    fix bob code ass unblock user

--- blocked_user.php
+++ blocked_user.php
@@ -4,42 +4,15 @@ include("components/header.php");
 include("components/navbar.php");
 
 // Check if the current user is an admin
-$is_admin = false;
+$is_admin = $_SESSION['is_admin'];
 
-// Retrieve the current user's information
-if (isset($_SESSION['user_id'])) {
-    $user_id = $_SESSION['user_id'];
-    $query = "SELECT * FROM users WHERE id = $user_id";
-    $result = mysqli_query($connection, $query);
+// Handle the form submission
+if (isset($_POST["update"])) {
+    $user_id = $_POST['user_id'];
 
-    if (!$result) {
-        die('Error querying database: ' . mysqli_error($connection));
-    }
-
-    $user = mysqli_fetch_assoc($result);
-
-    if ($user && $user['is_admin'] == 1) {
-        $is_admin = true;
-    }
+    // Update the user's is_blocked field to 0
+    $result = mysqli_query($connection, "UPDATE users SET is_blocked = 0 WHERE id = $user_id");
 }
-
-// Retrieve a list of all users
-$users = array();
-
-$query = "SELECT * FROM users";
-$result = mysqli_query($connection, $query);
-
-while ($row = mysqli_fetch_assoc($result)) {
-    $users[] = $row;
-}
-
-// Debugging output
-var_dump(isset($_SESSION['user_id']));
-var_dump($result);
-var_dump(mysqli_fetch_assoc($result));
-var_dump(isset($user));
-var_dump($is_admin);
-var_dump(isset($user['is_admin']) ? $user['is_admin'] : null);
 ?>
 
 
@@ -62,30 +35,23 @@ var_dump(isset($user['is_admin']) ? $user['is_admin'] : null);
             </tr>
         </thead>
         <tbody>
-            <?php
-            mysqli_data_seek($result, 0);
-            foreach ($users as $user) :
-            ?>
-                <?php if ($user['is_blocked'] == 1) : ?>
+        <?php
+            $data = mysqli_query($connection, "SELECT * FROM users WHERE is_blocked = 1");
+            while ($info = mysqli_fetch_array($data)) { ?>
                     <tr>
-                        <td class="border px-4 py-2"><?php echo $user['id']; ?></td>
-                        <td class="border px-4 py-2"><?php echo $user['name']; ?></td>
-                        <td class="border px-4 py-2"><?php echo $user['ic']; ?></td>
-                        <td class="border px-4 py-2"><?php echo $user['is_blocked'] ? '<span class="bg-red-500 text-white px-2 rounded">Blocked</span>' : '<span class="bg-green-500 text-white px-2 rounded">Active</span>'; ?></td>
+                        <td class="border px-4 py-2"><?php echo $info['id']; ?></td>
+                        <td class="border px-4 py-2"><?php echo $info['name']; ?></td>
+                        <td class="border px-4 py-2"><?php echo $info['ic']; ?></td>
+                        <td class="border px-4 py-2"><?php echo $info['is_blocked'] ? '<span class="bg-red-500 text-white px-2 rounded">Blocked</span>' : '<span class="bg-green-500 text-white px-2 rounded">Active</span>'; ?></td>
                         <td class="border px-4 py-2">
-                            <?php if ($is_admin) : ?>
-                                <form action="unblock_user.php" method="post" class="inline">
-                                    <input type="hidden" name="user_id" value="<?php echo $user['id']; ?>">
-                                    <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Unblock</button>
-                                </form>
-                            <?php else : ?>
-                                <span class="text-gray-500">Not Available</span>
-                            <?php endif; ?>
+                            <form method="post" class="inline">
+                                <input type="hidden" name="user_id" value="<?php echo $info['id']; ?>">
+                                <button type="submit" name="update" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Unblock</button>
+                            </form>
                         </td>
                     </tr>
-                <?php endif; ?>
-            <?php endforeach; ?>
         </tbody>
+        <?php } ?>
     </table>
 
 
