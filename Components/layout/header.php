beb15fb318cb33169d82d55cff6321bd2838cbb7
Apitpr0 <apitpro123@gmail.com>
2023-05-11 00:21:36 +0800

    formatting

--- Components/layout/header.php
+++ Components/layout/header.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SKLY Counselling Management System</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SKLY Counselling Management System</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
     tailwind.config = {
-        theme: {
-    colors: {
-        background: '#EBC7E6',
-        card: '#645CBB',
-        primary: '#F5F3FF',
-        text: '#FFFFFF',
-    },
-  },
+      theme: {
+        colors: {
+          background: '#EBC7E6',
+          card: '#645CBB',
+          primary: '#F5F3FF',
+          text: '#FFFFFF',
+        },
+      },
     }
   </script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
 </head>
 <body>
