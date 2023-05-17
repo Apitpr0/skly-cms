37250f4b3adab121000d970cb824f5a378c52940
Apitpr0 <apitpro123@gmail.com>
2023-05-17 23:26:32 +0800

    add check for ic number not exist

--- reset_password.php
+++ reset_password.php
@@ -9,25 +9,31 @@ if (isset($_POST['submit'])) {
 
     // Check if the passwords match
     if ($password != $confirm_password) {
-        echo "Passwords do not match";
-        exit;
-    }
-
-    // Hash the password using SHA-512
-    $hashed_password = hash('sha512', $password);
-
-    // Update the user's password in the database
-    $sql = "UPDATE users SET password='$hashed_password' WHERE ic='$ic'";
-    if (mysqli_query($connection, $sql)) {
-        echo '<div class="bg-green-500 text-white font-bold py-2 px-4 rounded mb-4">Kata Laluan Berjaya Dikemaskini</div>';
+        echo '<div class="bg-red-500 text-white font-bold py-2 px-4 rounded mb-4">Passwords do not match</div>';
     } else {
-        echo '<div class="bg-red-500 text-white font-bold py-2 px-4 rounded mb-4">Kata Laluan Tidak Berjaya Dikemaskini</div>' . mysqli_error($connection);
+        // Hash the password using SHA-512
+        $hashed_password = hash('sha512', $password);
+
+        // Check if the IC number exists in the database
+        $sql = "SELECT * FROM users WHERE ic='$ic'";
+        $result = mysqli_query($connection, $sql);
+        if (mysqli_num_rows($result) == 0) {
+            echo '<div class="bg-red-500 text-white font-bold py-2 px-4 rounded mb-4">Nombor IC tidak dijumpai dalam pangkalan data</div>';
+        } else {
+            // Update the user's password in the database
+            $sql = "UPDATE users SET password='$hashed_password' WHERE ic='$ic'";
+            if (mysqli_query($connection, $sql)) {
+                echo '<div class="bg-green-500 text-white font-bold py-2 px-4 rounded mb-4">Kata Laluan Berjaya Dikemaskini</div>';
+            } else {
+                echo '<div class="bg-red-500 text-white font-bold py-2 px-4 rounded mb-4">Kata Laluan Tidak Berjaya Dikemaskini</div>' . mysqli_error($connection);
+            }
+        }
     }
-    
 }
 
 mysqli_close($connection);
 ?>
+
 <div class="max-w-md mx-auto my-8">
     <h1 class="text-2xl font-bold mb-4">Lupa Kata Laluan?</h1>
     <form action="" method="post" class="space-y-4">
@@ -36,11 +42,11 @@ mysqli_close($connection);
             <input type="text" id="ic" name="ic" required class="w-full px-4 py-2 border border-gray-300 rounded-md">
         </div>
         <div>
-            <label for="password" class="block font-medium">Kata Laluan baharu:</label>
+            <label for="password" class="block font-medium">Kata Laluan Baharu:</label>
             <input type="password" id="password" name="password" required class="w-full px-4 py-2 border border-gray-300 rounded-md">
         </div>
         <div>
-            <label for="confirm_password" class="block font-medium">Sahkan Kata Laluan baharu:</label>
+            <label for="confirm_password" class="block font-medium">Sahkan Kata Laluan Baharu:</label>
             <input type="password" id="confirm_password" name="confirm_password" required class="w-full px-4 py-2 border border-gray-300 rounded-md">
         </div>
         <button type="submit" name="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Set Semula Kata Laluan</button>
@@ -48,7 +54,6 @@ mysqli_close($connection);
     <div class="mt-4 text-center">
         <a href="login.php" class="text-blue-500 hover:text-blue-700 font-medium">Kembali ke Log Masuk</a>
     </div>
-
 </div>
 
 <?php
