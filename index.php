7df09da8f91949d23d803bd728523da95033e4c6
Apitpr0 <apitpro123@gmail.com>
2023-06-12 22:21:44 +0800

    some random push

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 include('Components/db/db_connection.php');
 include('Components/header.php');
 include('Components/navbar.php');
-
 ?>
 <section class="">
 	<div class="container flex flex-col justify-center  p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-around">
